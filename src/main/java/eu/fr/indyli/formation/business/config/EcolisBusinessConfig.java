1dd0f6e2f1b330ab4c137dd53a72eea8796621da
cyril <cyril.tingah@indyli-services.com>
2021-12-16 09:30:54 +0100

    restauration

---
+++ src/main/java/eu/fr/indyli/formation/business/config/EcolisBusinessConfig.java
@@ -0,0 +1,13 @@
+package eu.fr.indyli.formation.business.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+//@EntityScan("eu.fr.indyli.formation.business.entity")
+//@ComponentScan(basePackages="eu.fr.indyli.formation.business.ecolis.service")
+//@EnableJpaRepositories("eu.fr.indyli.formation.business.dao")
+//@Import({EcolisDbConfig.class,TacheDbConfig.class})
+@Import({EcolisDbConfig.class})
+public class EcolisBusinessConfig {
+}
